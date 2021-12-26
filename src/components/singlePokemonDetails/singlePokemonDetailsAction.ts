994e037c4e220337a2c0de24ce961a36155d6e03
Muwonge Nicholus <nicholusmuwonge@gmail.com>
2021-12-26 14:13:01 +0300

    Adds ability to navigate to given pokemon without first loading them all up.

--- src/components/singlePokemonDetails/singlePokemonDetailsAction.ts
+++ src/components/singlePokemonDetails/singlePokemonDetailsAction.ts
@@ -1,26 +1,32 @@
 import { Dispatch } from "redux";
 import Api from "../../axios/api";
-import { imageBaseUrl, PokemonDispatchTypes, POKEMON_SUCCESS } from "../allPokemons/actionTypes";
+import {
+  imageBaseUrl,
+  PokemonDispatchTypes,
+  POKEMON_SUCCESS,
+} from "../allPokemons/actionTypes";
 
 export const getPokemonDetailsByName = (
-    pokemonName: string,
-    setNotFound: (state: boolean) => void
-  ) => async (dispatch: Dispatch<PokemonDispatchTypes>): Promise<void> => {
-    try {
-      const response = await Api.getSinglePokemonDetailsByName(pokemonName);
-      const responseCopy = {...response.data, image: imageBaseUrl+`${response.data.id}.png`}
-      dispatch({
-        type: POKEMON_SUCCESS,
-        payload: [responseCopy],
-        count: [responseCopy].length,
-      });
-    } catch (err: any) {
-      if (
-        (err.response && err.response.status === 404) ||
-        err.response.status === 301
-      ) {
-        setNotFound(true);
-      }
+  pokemonName: string,
+  setNotFound: (state: boolean) => void
+) => async (dispatch: Dispatch<PokemonDispatchTypes>): Promise<void> => {
+  try {
+    const response = await Api.getSinglePokemonDetailsByName(pokemonName);
+    const responseCopy = {
+      ...response.data,
+      image: imageBaseUrl + `${response.data.id}.png`,
+    };
+    dispatch({
+      type: POKEMON_SUCCESS,
+      payload: [responseCopy],
+      count: [responseCopy].length,
+    });
+  } catch (err: any) {
+    if (
+      (err.response && err.response.status === 404) ||
+      err.response.status === 301
+    ) {
+      setNotFound(true);
     }
-  };
-  
+  }
+};
